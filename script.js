fc32b796c018c9f456e46ce04fd0cdc52746a0d7
Rudi YK <rudiyuri86@gmail.com>
2026-05-05 09:23:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function cekLogin() {
+            const user = document.getElementById('username').value;
+            const pass = document.getElementById('password').value;
+
+            if (user === 'admin' && pass === '1234') {
+                window.location.href = 'index.html';
+            } 
+            else {
+                    alert('Username atau Password salah');
+            }
+        }
+
