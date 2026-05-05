2473146a294f9383e9fff9d95ecf3d71f5aa9bbe
Rudi YK <rudiyuri86@gmail.com>
2026-05-05 12:08:58 +0700

    penambahan code js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function cekLogin() {
 
             if (user === 'admin' && pass === '1234') {
                 sessionStorage.setItem('loggedIN' , 'true') // simpan status logis
+                window.location.href = 'login.html';
                 window.location.href = 'index.html';
             } 
             else {
